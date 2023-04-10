d517993eba69ed4922c5f3393d03cf61c77dfed6
Shlok Kumar Suman <shloksuman01@gmail.com>
2023-04-10 13:12:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,55 @@ function addNotes(){
         alert('Add your note')
         return ;
     }
+
+    
+    
+
+    const notesObj = {
+        title : addtitle.value  ,
+        text : addtext.value , 
+    }
+    notes.push(notesObj);
+
+    addtitle.value = "";
+    addtext.value = "";
+
+    //update the storage
+    localStorage.setItem('notes' , JSON.stringify(notes));
+    showNotes();
+}
+
+function showNotes() {
+    let notesHTML = '' ;
+    let notes = localStorage.getItem('notes') ;
+    if(notes === null){
+        return ;
+    } else {
+        notes = JSON.parse(notes);
+    }
+    for(let i=0; i< notes.length ; i++){
+        notesHTML += `<div class="note">
+        <button class="deletenote" id=${i} onclick="deleteNote(${i})" >Delete</button>
+        <div class="title">${notes[i].title === "" ? 'Note' : notes[i].title}</div>
+        <div class="text">${notes[i].text}</div>
+    </div>
+        `
+    }
+
+    notesDiv.innerHTML = notesHTML ;
+}
+
+function deleteNote(ind){
+    let notes = localStorage.getItem('notes') ;
+    if(notes === null){
+        return ;
+    } else {
+        notes = JSON.parse(notes);
+    }
+
+    notes.splice(ind , 1);
+    localStorage.setItem('notes', JSON.stringify(notes)) ;
+    showNotes();
+}
+
+addnotebutton.addEventListener('click' , addNotes);
